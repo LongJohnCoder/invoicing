2dca3caf26536e801ba3c405799f9ecc0ef936f5
prodipto <prodip211085@gmail.com>
2016-04-28 14:30:14 +0530

    Adding invoice section

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,3 +12,4 @@
 */
 
 Route::get('/', 'HomeController@index');
+Route::get('client/invoice/{id}','ClientController@invoice');
---
+++ app/Model/Invoice.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    //
+    public $timestamps = true;
+	protected $table = 'invoice';
+	protected $guarded = array();
+	public function invoice_items() {
+        return $this->hasMany('App\Model\InvoiceItem', 'invoice_id','invoice_id');
+        }
+    public function user_details(){
+        return $this->hasOne('App\Model\User', 'user_id','id');
+    }
+}
