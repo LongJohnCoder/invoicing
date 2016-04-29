17f946ec12b958faecc4941d865656bed3d1d1c1
prodipto <prodip211085@gmail.com>
2016-04-29 17:19:33 +0530

    Changes in Invoice List Page

--- resources/views/home/invoiceDetails.blade.php
+++ resources/views/home/invoiceDetails.blade.php
@@ -9,71 +9,82 @@
       </h1>   
     </section>
     <section class="content">
+      
       <div class="row">
-        <!-- right column -->
-        <div class="col-md-12">
-          <!-- Horizontal Form -->
-          <div class="box box-info">
-            <div class="box-header with-border">
-              <h3 class="box-title">Basic info</h3>
-          </div>
-          <div class="box-body">
-            <table class="table table-bordered">
-              <tr>
-                <th>Invoice #</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Items x Quantity</th>
-                <th>Tax Rate(%)</th>
-                <th>Price</th>
-                <th>Created At</th>
-                <th>Payment Status</th>
-                <th>Payment Date</th>
-              </tr>
-              @foreach($user_details as $user)
-              <tr>
-                <td>
-                  {{$user->invoice_id}}
-                </td>
-                <td>
-                  {{$user->user_details->name}}
-                </td>
-                <td>
-                  {{$user->user_details->email}}
-                </td>
-                <td>
-                @foreach($user->invoice_items as $items)
-                  <button class="btn btn-default" style="margin-bottom: 10px;">{{ $items->name }} x {{ $items->qty}}</button>
-                @endforeach
-                </td>
-                <td>
-                  {{ $user->tax_rate }}
-                </td>
-                <td>
-                  {{ $user->total = $user->total+ ($user->total*($user->tax_rate/100)) }} 
-                </td>
-                <td>
-                  {{date(" F j,Y ", strtotime($user->created_at))}}
-                </td>
-                <td>
-                @if ($user->payment_status == 0)
-                  Pending
-                @else
-                  Paid up
-                @endif
-                </td>
-                <td>
-                  @if($user->payment_status == 0)
-                    Not Yet Paid
-                  @else
-                    {{ date(" F j,Y ", strtotime($user->updated_at)) }}
-                  @endif
-                </td>
-              </tr>
-              @endforeach
-            </table>
-          </div><!-- /.box-body -->
-        </div>
+            <div class="col-xs-12">
+              <div class="box">
+                <div class="box-header">
+                  <h3 class="box-title">Invoice List</h3>
+                  <div class="box-tools">
+                    
+                  </div>
+                </div><!-- /.box-header -->
+                <div class="box-body table-responsive no-padding">
+                  <table class="table table-hover">
+                    <tr>
+                      <th>Invoice #</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Items x Quantity</th>
+                      <th>Tax Rate(%)</th>
+                      <th>Price</th>
+                      <th>Created At</th>
+                      <th>Payment Status</th>
+                      <th>Payment Date</th>
+                      <th>Link</th>
+                      <th>Details</th>
+                    </tr>
+                    @foreach($user_details as $user)
+                        <tr>
+                            <td>
+                            {{$user->invoice_id}}
+                            </td>
+                            <td>
+                            {{$user->user_details->name}}
+                            </td>
+                            <td>
+                            {{$user->user_details->email}}
+                            </td>
+                            <td>
+                            @foreach($user->invoice_items as $items)
+                              <button class="btn btn-default" style="margin-bottom: 10px;">{{ $items->name }} x {{ $items->qty}}</button>
+                            @endforeach
+                            </td>
+                            <td>
+                            {{ $user->tax_rate }}
+                            </td>
+                            <td>
+                            {{ $user->total = $user->total+ ($user->total*($user->tax_rate/100)) }} 
+                            </td>
+                            <td>
+                            {{date(" F j,Y ", strtotime($user->created_at))}}
+                            </td>
+                            <td>
+                            @if ($user->payment_status == 0)
+                              Pending
+                            @else
+                              Paid up
+                            @endif
+                            </td>
+                            <td>
+                            @if($user->payment_status == 0)
+                              Not Yet Paid
+                            @else
+                              {{ date(" F j,Y ", strtotime($user->updated_at)) }}
+                            @endif
+                            </td>
+                            <td>
+                              <a href="{{url('/')}}/invoice-created/{{base64_encode($user->invoice_id)}}"><i class="fa fa-fw fa-link"></i></a>
+                            </td>
+                            <td>
+                              <a href="{{url('/')}}/client/invoice/{{base64_encode($user->invoice_id)}}"><i class="fa fa-fw fa-list-alt"></i></a>
+                            </td>
+                        </tr>
+                    @endforeach
+                  </table>
+                </div><!-- /.box-body -->
+              </div><!-- /.box -->
+            </div>
       </div>
     </section>
   </div>
