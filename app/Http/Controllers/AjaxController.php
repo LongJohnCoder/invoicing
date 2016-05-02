8456a87b33af4e16d14e1e4311bd0dc05e7578e9
prodipto <prodip211085@gmail.com>
2016-05-02 17:29:19 +0530

    dashboard graph FIRST PHASE

---
+++ app/Http/Controllers/AjaxController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Model\Invoice;
+
+use App\Model\InvoiceItem;
+
+use App\Model\User;
+
+use Mail;
+
+class AjaxController extends Controller
+{
+    //
+    public function getWeekGraph(){
+
+    	$sevendayearly=date("Y-m-d", strtotime('-7 days'));
+    	$todaysdate=date("Y-m-d");
+    	$user_details = Invoice::where('created_at','>=',$sevendayearly)->where('created_at','<=',$todaysdate)->with('invoice_items', 'user_details')->orderBy('created_at', 'desc')->get();
+    	//dd($user_details);
+    	$inc='1 days';
+    	$per=array();
+    	for($i=0;$i<8;$i++){
+    		//echo $i;
+    		$ordate=date('Y-m-d', strtotime('+'.$i.' days', strtotime($sevendayearly)));
+    		$sum =Invoice::whereDate('created_at', '=', $ordate)->get();
+    		$sumc =Invoice::whereDate('created_at', '=', $ordate)->where('payment_status','=',1)->get();
+    		//dd($sum);
+			$per[$ordate]['tot']=0;
+			$per[$ordate]['ctot']=0;
+    		foreach ($sum as  $Invoice) {
+    			if($per[$ordate]['tot']==0){
+    				$per[$ordate]['tot']=$Invoice->total+($Invoice->total*($Invoice->tax_rate/100));
+    			}
+    			else{
+    				$per[$ordate]['tot']=$per[$ordate]['tot']+$Invoice->total+($Invoice->total*($Invoice->tax_rate/100));
+    			}
+    		}
+    		foreach ($sumc as  $Invoice) {
+    			if($per[$ordate]['ctot']==0){
+    				$per[$ordate]['ctot']=$Invoice->total+($Invoice->total*($Invoice->tax_rate/100));
+    			}
+    			else{
+    				$per[$ordate]['ctot']=$per[$ordate]['ctot']+$Invoice->total+($Invoice->total*($Invoice->tax_rate/100));
+    			}
+    		}
+
+    	}
+    	$result=array();
+    	foreach ($per as $key => $raw) {
+    		$result['intvalu'][]=$key;
+    		$result['totvalu'][]=$raw['tot'];
+    		$result['recvalu'][]=$raw['ctot'];
+    	}
+    	return json_encode($result);
+    }
+}
