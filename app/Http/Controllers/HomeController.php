e32d0b84b2a85e733cdaf6dbb508cbd5e1e74a7e
subhankartier5 <roysubho687@gmail.com>
2016-06-08 17:20:08 +0530

    premium membership done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -457,80 +457,114 @@ class HomeController extends Controller
     return view('index');
   }
   public function getRegister() {
-    return view('register');
+    $super_admin_account = Admin::where('use_my_account', 1)->where('admin_type', 1)->with('payment_keys')->first();
+      return view('register', compact('super_admin_account'));
   }
   public function postRegister(Request $request) {
-    //dd($request);
     $name = $request->usr_name;
     $email = $request->usr_email;
-    //$gender = $request->usr_gender;
     $password = $request->conf_pass;
     $membership_status = $request->select_membership;
-    if ($membership_status == 1) {
-      $return_lastInserted_id = $this->RegisterUser($name, $email, $password, $membership_status);
-      if ($return_lastInserted_id) {
-        //dd($return_lastInserted_id);
-        if ($return_lastInserted_id == 302) {
-          return redirect()->route('register')->with('custom_err', 'Email Already exists try another one');
-        }
-        else
-        {
-          //for basic membership no payment needed so make it 1
-          $search = Admin::where('id' ,$return_lastInserted_id)->first();
-          if ($search) {
-             $search->payment_status = 1;
-             $search->save();
-             return redirect()->route('admin-login')->with('success_registration', 'You have successfully registerted.');
-           } 
-           else
-           {
-            return redirect()->route('register')->with('custom_err', 'could not find the username in our database');
-           }
-        }
+    $stripeToken = $request->stripeToken;
+    if ($stripeToken) 
+    {
+      //premium member here
+      $return = $this->RegisterUser($name, $email, $password, $membership_status, $stripeToken);
+      if ($return == 302) {
+        return redirect()->route('register')->with('custom_err', 'Email Already exists try another one');
       }
       else
       {
-        return redirect()->route('register')->with('custom_err', 'Some Error occured please contact system adminstrator');
-      }
-    } 
-
-    //pro membership
-    elseif ($membership_status == 2) {
-      $return_lastInserted_id = $this->RegisterUser($name, $email, $gender, $password, $membership_status);
-      if ($return_lastInserted_id) {
-        if ($return_lastInserted_id == 302) {
-          return redirect()->route('register')->with('custom_err', 'Email Already exists try another one');
+        $last_inserted_id = $return;
+        //now if last inserted id here proceed to payment
+        if ($last_inserted_id) {
+          $complete_register = $this->doPayment($last_inserted_id, $membership_status, $stripeToken);
+          return $complete_register;
         }
         else
         {
-          return redirect()->route('post-registration-payment', ['membership' => 'pro', 'last_inserted_id' => base64_encode($return_lastInserted_id)]);
+          return redirect()->route('register')->with('custom_err', 'some error occured please try again later');
         }
       }
-      else
-      {
-        return redirect()->route('register')->with('custom_err', 'Some Error occured please contact system adminstrator');
-      }
-
     }
-
-    //gold membership
     else
     {
-      $return_lastInserted_id = $this->RegisterUser($name, $email, $gender, $password, $membership_status);
-      if ($return_lastInserted_id) {
-        if ($return_lastInserted_id == 302) {
-          return redirect()->route('register')->with('custom_err', 'Email Already exists try another one');
-        }
-        else
-        {
-          return redirect()->route('post-registration-payment', ['membership' => 'gold', 'last_inserted_id' => base64_encode($return_lastInserted_id)]);
-        }
+      //basic member here
+      $return = $this->RegisterUser($name, $email, $password, $membership_status, 0);
+      if ($return == 302) 
+      {
+        return redirect()->route('register')->with('custom_err', 'Email Already exists try another one');
       }
       else
       {
-        return redirect()->route('register')->with('custom_err', 'Some Error occured please contact system adminstrator');
+        $last_inserted_id = $return;
+        //now if last inserted id here proceed to login
+        if ($last_inserted_id) {
+          return redirect()->route('admin-login')->with('success_registration', 'You have successfully registerted.');
+        }
+        else
+        {
+         return redirect()->route('register')->with('custom_err', 'some error occured please try again later'); 
+        }
+
       }
     }
+  }
+
+  private function RegisterUser($name, $email, $password, $membership_status, $stripeToken) {
+      $search_email = Admin::where('email', $email)->first();
+      if ($search_email) {
+        return 302;
+      } else {
+        $admin = new Admin();
+        $admin->email = $email;
+        $admin->password = bcrypt($password);
+        $admin->admin_type = 0;
+        $admin->block_status = 0;
+        $admin->payment_status = 0;
+        $admin->use_my_account = 0;
+        if ($admin->save()) {
+          $admin_details = new AdminDetail();
+          $admin_details->admin_id =$admin->id;
+          $admin_details->name = $name;
+          $admin_details->detail = 'Please write something about you';
+          $admin_details->membership = $membership_status;
+          if ($admin_details->save()) {
+            return $admin_details->admin_id;
+          }
+        } else {
+            return false;
+        }
+      }
+  }
+  private function doPayment($admin_id, $membership_status, $stripeToken) {
+    $keys = Admin::where('use_my_account', 1)->where('admin_type', 1)->with('payment_keys')->first();
+    $secret_key = $keys->payment_keys->key_second;
+    $charge_amount = ($membership_status == 2 ? 10 : 20) ;
+    $stripe = Stripe::make($secret_key);
+    $charge = $stripe->charges()->create([
+      'source' => $stripeToken,
+      'currency' => 'USD',
+      'amount'   => $charge_amount,
+    ]);
+    if($charge['status']=="succeeded")
+    {
+      $search_admin = Admin::find($admin_id);
+      if ($search_admin) {
+        $search_admin->payment_status = 1;
+        $search_admin->save();
+        return redirect()->route('admin-login')->with('success_registration', 'Hey! you have successfully registered as premium member');
+      }
+      else
+      {
+        return redirect()->route('admin-login')->with('fail', 'Failed to search an admin to update please check database');
+      }
+    } 
+    else
+    {
+      return redirect()->route('register')->with('fail', 'Payment Failed please close the browser and open then try it again.');
+    }
+
   }
   public function BanUser($id) {
     $id_ban = base64_decode($id);
@@ -552,30 +586,6 @@ class HomeController extends Controller
     {
       return redirect()->route('dashboard')->with('block_status_er', 'Failed to block the user');
     }
-  }
-  private function RegisterUser($name, $email, $password, $membership_status) {
-      $search_email = Admin::where('email', $email)->first();
-      if ($search_email) {
-        return 302;
-      } else {
-        $admin = new Admin();
-        $admin->email = $email;
-        $admin->password = bcrypt($password);
-        $admin->admin_type = 0;
-        if ($admin->save()) {
-          $admin_details = new AdminDetail();
-          $admin_details->admin_id =$admin->id;
-          $admin_details->name = $name;
-          $admin_details->detail = 'Please write something about you';
-          //$admin_details->gender = $gender;
-          $admin_details->membership = $membership_status;
-          if ($admin_details->save()) {
-            return $admin_details->admin_id;
-          }
-        } else {
-            return false;
-        }
-      }
   }
   public function postMembershipPayment($membership, $last_inserted_id) {
     //dd('err');
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -17,7 +17,7 @@
       @endif
       <div class="register-box-body">
         <p class="login-box-msg">Register a new membership</p>
-        <form action="{{ route('postRegister') }}" method="post">
+        <form action="{{ route('postRegister') }}" method="post" id="payment-form">
           <div class="form-group has-feedback">
             <input type="text" class="form-control" placeholder="Full name" name="usr_name" required>
             <span class="glyphicon glyphicon-user form-control-feedback"></span>
@@ -36,11 +36,37 @@
           </div>
           <div class="form-group has-feedback">
               <select class="form-control" id="select_membership" name="select_membership" required>
+                <option value="">Select membersip</option>
                 <option value="1">Basic</option>
                 <option value="2">Professional</option>
                 <option value="3">Gold</option>
               </select>
           </div>
+          @if($super_admin_account!=null)
+            @if($super_admin_account->payment_keys != null)
+            <div class="form-group has-feedback" id="payment_div" style="display: none;">
+              <span class="payment-errors"></span>
+              <!-- work payment form ---------------->
+              <input type="text" size="20" data-stripe="number" class="form-control" placeholder="card number"/><br/>
+              <span class="glyphicon glyphicon-credit-card form-control-feedback"></span>
+              <div class="col-xs-4">
+                <input type="text" size="2" data-stripe="exp_month" class="form-control" placeholder="MM" style="margin-left: -19%;" />
+              </div>
+              <div class="col-xs-8">
+                <input type="text" size="2" data-stripe="exp_year" class="form-control" placeholder="YYYY" style="width: 127%; margin-left: -19%;" />
+              </div>
+              <input type="text" size="4" data-stripe="cvc" class="form-control" style="margin-top: 16%;" placeholder="cvc">
+            </div>
+            @else
+              <div class="form-group has-feedback" style="color: red;">
+                <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Super Admin has not yet set up payment account try basic for now.
+              </div>
+            @endif
+          @else
+            <div class="form-group has-feedback" style="color: red;">
+              <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> No Super Admin Exists Try Basic for now.
+            </div>
+          @endif
           <div class="row">
             <div class="col-xs-8">
               <div class="checkbox icheck">
@@ -61,4 +87,80 @@
     </div><!-- /.register-box -->
     @include('section.admin-lte-footer')
   </body>
+  <script type="text/javascript">
+    /* payment form slide down slide up script*/
+    $(document).ready(function(){
+      $('#select_membership').click(function(){
+        var value = $('#select_membership').val();
+        if($.trim(value) > 1) {
+          
+          $('#payment_div').slideDown();
+        }
+        else
+        {
+          $('#payment_div').slideUp();
+        }
+      });
+    });
+  </script>
+  <!--stripe js-->
+  @if($super_admin_account->payment_keys !=null)
+    <script type="text/javascript">
+      $(document).ready(function(){
+      //set dyanmic publishable key here
+        $('#select_membership').click(function(){
+          var value = $('#select_membership').val();
+          if ($.trim(value) > 1) 
+          {
+            Stripe.setPublishableKey('{{$super_admin_account->payment_keys->key_first}}');
+          }
+          else
+          {
+            console.log('Basic Member');
+            return false;
+          }
+        });
+      });
+      $(function() {
+        var $form = $('#payment-form');
+        $form.submit(function(event) {
+          // Disable the submit button to prevent repeated clicks:
+          $form.find('.submit').prop('disabled', true);
+      
+          // Request a token from Stripe:
+          Stripe.card.createToken($form, stripeResponseHandler);
+      
+          // Prevent the form from being submitted:
+          return false;
+        });
+      });
+      function stripeResponseHandler(status, response) {
+      // Grab the form:
+      var $form = $('#payment-form');
+      if (response.error) { // Problem!
+      // Show the errors on the form:
+      $form.find('.payment-errors').text(response.error.message);
+      $form.find('.submit').prop('disabled', false); // Re-enable submission
+    
+      } else { // Token was created!
+    
+      // Get the token ID:
+      var token = response.id;
+      //console.log(token)
+      //console.log(response);
+      // Insert the token ID into the form so it gets submitted to the server:
+      $form.append($('<input type="hidden" name="stripeToken">').val(token));
+    
+      // Submit the form:
+      $form.get(0).submit();
+    }
+    };
+    </script>
+  @else
+    <script type="text/javascript">
+      $(document).ready(function(){
+        console.log("Can't Accpet your payment right now");
+      });
+    </script>
+  @endif
 </html>
